e22f41bcb966efeae182711a1f4b440a4918f5b9
ryansummerlin <ryansummerlin777@gmail.com>
2023-07-06 22:22:41 -0700

    added functionality for all cart buttons, need to finish change value button

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,4 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { incrementProduce, decrementProduce, removeProduce, changeCount } from '../../store/cart';
 
 function CartItem({ item }) {
   const [count, setCount] = useState(item.count);
@@ -7,6 +9,39 @@ function CartItem({ item }) {
     setCount(item.count);
   }, [item.count]);
 
+  const dispatch = useDispatch();
+
+  const onClickPlus = function() {
+    dispatch(incrementProduce(item));
+  }
+
+  const onClickMinus = () => {
+    dispatch(decrementProduce(item));
+    // if (count <= 1) {
+    //   removeItem();
+    // }
+  }
+
+  // const removeItem = () => {
+  //   dispatch(removeProduce(item));
+  // }
+
+  const removeItem = useCallback(() => {
+    dispatch(removeProduce(item));
+  }, [dispatch, item]);
+
+  const handleCountChange = (e) => {
+    // need to add code to remove an item when it goes below zero AND gets clicked off of. Can't do it just when it goes to
+    // zero or it will remove the item every time I try to change the count
+    dispatch(changeCount(item, e.target.value));
+  }
+
+  useEffect(() => {
+    if (count <= 0) {
+      removeItem();
+    }
+  }, [count, removeItem]);
+
   return (
     <li className="cart-item">
       <div className="cart-item-header">{item.name}</div>
@@ -14,19 +49,23 @@ function CartItem({ item }) {
         <input
           type="number"
           value={count}
+          onChange={handleCountChange}
         />
         <button
           className="cart-item-button"
+          onClick={onClickPlus}
         >
           +
         </button>
         <button
           className="cart-item-button"
+          onClick={onClickMinus}
         >
           -
         </button>
         <button
           className="cart-item-button"
+          onClick={removeItem}
         >
           Remove
         </button>
--- src/store/cart.js
+++ src/store/cart.js
@@ -1,5 +1,9 @@
 
 const ADD = 'cart/ADD';
+const INCREMENT = 'cart/INCREMENT';
+const DECREMENT = 'cart/DECREMENT';
+const REMOVE = 'cart/REMOVE';
+const CHANGE_COUNT = 'cart/CHANGE_COUNT';
 
 export const addProduce = (produce) => {
     return {
@@ -8,16 +12,60 @@ export const addProduce = (produce) => {
     }
 }
 
+export const incrementProduce = (produce) => {
+    return {
+        type: INCREMENT,
+        produce
+    }
+}
+
+export const decrementProduce = (produce) => {
+    return {
+        type: DECREMENT,
+        produce
+    }
+}
+
+export const removeProduce = (produce) => {
+    return {
+        type: REMOVE,
+        produce
+    }
+}
+
+export const changeCount = (produce, count) => {
+    return {
+        type: CHANGE_COUNT,
+        produce,
+        count
+    }
+}
+
 export default function cartReducer(state = {}, action) {
+    const newState = Object.assign({}, state);
+    let id;
+    if (action.produce) {
+        id = action.produce.id;
+    }
     switch(action.type) {
         case ADD:
-            const newState = Object.assign({}, state);
-            let id = action.produce.id;
             if (newState[id]) {
                 newState[id].count++;
             } else {
                 newState[id] = { id, count: 1 };
             }
+            return newState;
+        case INCREMENT:
+            newState[id].count++;
+            return newState;
+        case DECREMENT:
+            newState[id].count--;
+            return newState;
+        case REMOVE:
+            delete newState[id];
+            return newState;
+        case CHANGE_COUNT:
+            newState[id].count = action.count;
             return newState;
         default:
             return state;
